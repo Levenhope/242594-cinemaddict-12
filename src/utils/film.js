8f66ee02faebcdaef963b18ed4cf44abc5729c10
Надежда Левенкова <n.levenkova@webprofy.ru>
2020-09-25 22:58:25 +0300

    [WIP] Подлкючение и тест moment.js

--- src/utils/film.js
+++ src/utils/film.js
@@ -1,9 +1,3 @@
-export const getReadableDate = (date) => {
-  const formattedDate = new Intl.DateTimeFormat(`en-US`, {day: `numeric`, month: `long`, year: `numeric`});
-  const [{value: month}, , {value: day}, , {value: year}] = formattedDate.formatToParts(date);
-  return (`${day} ${month} ${year}`);
-};
-
 export const getReadableDuration = (minutes) => {
   return `${Math.floor(minutes / 60)}h${minutes % 60}m`;
 };
